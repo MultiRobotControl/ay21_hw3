c83aba12d211a6e37ccea835232ae07131ced909
Tully Foote <developer@ip-172-30-49-250.ec2.internal>
2021-04-12 22:50:54 -0700

    Exercise 3

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,6 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-4.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[2.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, -4.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-3, 0.0, 0.0]' '[0.0, 0.0, -1.5]'
+
